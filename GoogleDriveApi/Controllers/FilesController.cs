9b4fbb7b0477c4732f313f3a1e5d059b6b7d6881
chsami <sami.chkhachkhi@gmail.com>
2019-01-12 17:10:27 +0100

    (feat) Added get call for audio files from the google drive Added authentication towards the google drive api

---
+++ GoogleDriveApi/Controllers/FilesController.cs
@@ -0,0 +1,64 @@
+﻿using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using GoogleDriveApi.SecurityHelper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+/**
+ *
+ *String pageToken = null;
+do {
+  FileList result = driveService.files().list()
+      .setQ("mimeType='image/jpeg'")
+      .setSpaces("drive")
+      .setFields("nextPageToken, files(id, name)")
+      .setPageToken(pageToken)
+      .execute();
+  for (File file : result.getFiles()) {
+    System.out.printf("Found file: %s (%s)\n",
+        file.getName(), file.getId());
+  }
+  pageToken = result.getNextPageToken();
+} while (pageToken != null);
+ */
+
+
+
+namespace GoogleDriveApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        // GET api/files
+        [HttpGet]
+        public IEnumerable<File> Get()
+        {
+            var service = AuthenticateHelper.Authenticate();
+            FilesResource.ListRequest listRequest = service.Files.List();
+            //listRequest.PageSize = 10;
+            listRequest.Q = "mimeType='audio/mpeg'";
+            listRequest.Fields = "nextPageToken, files(id, name, mimeType)";
+
+            // List files.
+            IList<File> files = listRequest.Execute().Files;
+            return files;
+        }
+
+        // GET api/files/dowload
+        [HttpGet("download")]
+        public IEnumerable<File> Download()
+        {
+            var service = AuthenticateHelper.Authenticate();
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name, size, description)";
+
+            // List files.
+            IList<File> files = listRequest.Execute().Files;
+            return files;
+        }
+    }
+}
---
+++ GoogleDriveApi/SecurityHelper/AuthenticateHelper.cs
@@ -0,0 +1,48 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleDriveApi.SecurityHelper
+{
+    public static class AuthenticateHelper
+    {
+        static string ApplicationName = "YOURAPPLICATIONNAME";
+        static string RefreshToken = "INSERTHERE";
+
+        public static DriveService Authenticate()
+        {
+            var token = new TokenResponse
+            {
+                RefreshToken = RefreshToken
+            };
+            UserCredential credentials;
+            using (var stream = File.OpenRead("credentials.json"))
+            {
+                credentials = new UserCredential(new GoogleAuthorizationCodeFlow(
+                    new GoogleAuthorizationCodeFlow.Initializer()
+                    {
+                        ClientSecretsStream = stream,
+                        Scopes = new List<string>()
+                        {
+                            DriveService.Scope.Drive
+                        }
+                    }), Environment.UserName, token);
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credentials,
+                ApplicationName = ApplicationName,
+            });
+            return service;
+        }
+    }
+}
